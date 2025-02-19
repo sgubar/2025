e28892326b3ca2e9fc02a790330e067ed5826eab
Nikita6010 <84465278+Nikita6010@users.noreply.github.com>
2025-02-19 03:03:32 +0200

    Update dk_tool.h

--- DK41/Riabchuk Nikita/HW1/dk_tool.h
+++ DK41/Riabchuk Nikita/HW1/dk_tool.h
@@ -1,7 +1,5 @@
 #include<stdio.h>
-#include <math.h> 
-#include <stdlib.h>
 int db(int a, int b);
 int bd(int a, int b);
-int st(int x, int y);
+long long st(int x, int y);
 int counta(double number);
