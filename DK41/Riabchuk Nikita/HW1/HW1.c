f8ef333c6cb9f8159d6ef699550a524b8eca542f
Nikita6010 <84465278+Nikita6010@users.noreply.github.com>
2025-02-19 10:53:47 +0200

    Update HW1.c

--- DK41/Riabchuk Nikita/HW1/HW1.c
+++ DK41/Riabchuk Nikita/HW1/HW1.c
@@ -20,55 +20,32 @@ int main(int argc, char* argv[]) {
     const int size = 8;
     const int popa = 2;
     int a = (int)st(popa, size);
-    int num;
+    int num = -1;
     //num = rand() % a;
 
-    switch (argc){
-    case 1:
-         while (1) {
+    if (argc == 1 || argc == 2) {
+        if (argc == 2) {
+            num = atoi(argv[1]);
+        }
+            while (num >= a || num <=- 1) {
                 printf("Число:");
                 if (scanf("%i", &num) == 0) {
                     while (getchar() != '\n');
                     printf("еєеєєееєееє так діло не буде, забагато хочешь або роби менше %d або я тобі зламаю ноги\n", a);
                     continue;
                 }
-                if (num >= a) {
-                    printf("еєеєєееєееє так діло не буде, забагато хочешь або роби менше %d або я тобі зламаю ноги\n", a);
-                    continue;
-                }
-                break;
             }
-         break;
-        
-    case 2:
-       num = atoi(argv[1]);
-       if (num >= a) {
-           while (1) {
-               printf("Число:");
-               if (scanf("%i", &num) == 0) {
-                   while (getchar() != '\n');
-                   printf("еєеєєееєееє так діло не буде, забагато хочешь або роби менше %d або я тобі зламаю ноги\n", a);
-                   continue;
-               }
-               if (num >= a) {
-                   printf("еєеєєееєееє так діло не буде, забагато хочешь або роби менше %d або я тобі зламаю ноги\n", a);
-                   continue;
-               }
-               break;
-           }
-           break;
-       }
-       break;
-    default:
-        printf("ті дюрнії");
-        return -1;
-        break;
-    
-}
-    int 卍解вацаонима = dtb(num, size);
-    printf("%i", 卍解вацаонима);
-    printf("\n");
-    printf("%i", btd(卍解вацаонима, size));
-    printf("\n");
-    system("pause");
-    return 0;
+            int 卍解вацаонима = dtb(num, size);
+            printf("%i", 卍解вацаонима);
+            printf("\n");
+            printf("%i", btd(卍解вацаонима, size));
+            printf("\n");
+            system("pause");
+            return 0;
+
+        }
+        else {
+                printf("ті дюрнії");
+                return -1;
+            }
+            }
