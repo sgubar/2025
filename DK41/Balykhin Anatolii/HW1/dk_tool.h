c83d053c042b74dc733f3f5c9c3a89ff1a273eb7
Anatolii <balihinanatoliy@gmail.com>
2025-02-23 20:22:58 +0200

    Create dk_tool.h

---
+++ DK41/Balykhin Anatolii/HW1/dk_tool.h
@@ -0,0 +1,9 @@
+#ifndef DK_TOOL_H
+#define DK_TOOL_H
+
+
+void inputMatrix(int size, int matrix[size][size]);
+void printMatrix(int size, int matrix[size][size]);
+void multiply(int size, int A[size][size], int B[size][size], int result[size][size]);
+
+#endif 
