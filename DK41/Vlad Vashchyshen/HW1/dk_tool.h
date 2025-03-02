8169864d86f0625387d44c04dbc478b0517b18b8
mgVVkpi <vashchyshenkpisss@gmail.com>
2025-03-02 21:25:25 +0200

    Update dk_tool.h

--- DK41/Vlad Vashchyshen/HW1/dk_tool.h
+++ DK41/Vlad Vashchyshen/HW1/dk_tool.h
@@ -1,1 +1,10 @@
+#ifndef DK_TOOL_H
+#define DK_TOOL_H
 
+#define MAX_SIZE 10 // Максимальний розмір матриці
+
+void inputMatrix(int matrix[MAX_SIZE][MAX_SIZE], int rows, int cols);
+void printMatrix(int matrix[MAX_SIZE][MAX_SIZE], int rows, int cols);
+void subtractMatrices(int A[MAX_SIZE][MAX_SIZE], int B[MAX_SIZE][MAX_SIZE], int result[MAX_SIZE][MAX_SIZE], int rows, int cols);
+
+#endif
