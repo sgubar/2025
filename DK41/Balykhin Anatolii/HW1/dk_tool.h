ae4ba2852cb6742678323ef0d12e213136ac4ccd
Anatolii <balihinanatoliy@gmail.com>
2025-03-06 14:00:54 +0200

    Update dk_tool.h

--- DK41/Balykhin Anatolii/HW1/dk_tool.h
+++ DK41/Balykhin Anatolii/HW1/dk_tool.h
@@ -1,9 +1,10 @@
 #ifndef DK_TOOL_H
 #define DK_TOOL_H
 
-
-void inputMatrix(int size, int matrix[size][size]);
-void printMatrix(int size, int matrix[size][size]);
-void multiply(int size, int A[size][size], int B[size][size], int result[size][size]);
+int** createMatrix(int size);
+void inputMatrix(int size, int** matrix);
+void printMatrix(int size, int** matrix);
+void multiplyMatrices(int size, int** A, int** B, int** result);
 
 #endif // DK_TOOL_H
+
