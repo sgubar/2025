84105462423d5e51f4d2f8b7ea8a238a8e6501ff
Ілля Голуб <135114800+IllyaGolub0@users.noreply.github.com>
2025-03-13 12:05:45 +0200

    Update mathfunc.c

--- DK41/Golub Illya/Lab1/mathfunc.c
+++ DK41/Golub Illya/Lab1/mathfunc.c
@@ -1,6 +1,8 @@
 #include "mathfunc.h"
+#include <stdio.h>
 
 unsigned long long factorial(int n) {
+    if (n < 0) return 0;
     if (n <= 1) return 1;
     unsigned long long result = 1;
     for (int i = 1; i <= n; ++i) {
@@ -10,10 +12,15 @@ unsigned long long factorial(int n) {
 }
 
 double absolute(double value) {
-    if (value < 0) {
-        return -value;
-    } else {
-        return value;
-    }
+    return (value < 0) ? -value : value;
 }
 
+int check_input(void *value, const char *format) {
+    char buffer[256];
+    if (fgets(buffer, sizeof(buffer), stdin) != NULL) {
+        if (sscanf(buffer, format, value) == 1) {
+            return 1;
+        }
+    }
+    return 0;
+}
