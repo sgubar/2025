8d9b38852f758c2d44fe62eb54f1cfe8d189e2b3
Anatolii <balihinanatoliy@gmail.com>
2025-03-20 00:15:29 +0200

    Create func.h

---
+++ DK41/Balykhin Anatolii/Lab1/func.h
@@ -0,0 +1,14 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int stepin(int base, int exp);
+int suma(int a);
+double Q(int A, int B, int C);
+int absolute(int x);
+void get_input(int* A, int* B, int* C);
+void clear_variables(int* A, int* B, int* C);
+
+#endif // FUNC_H
