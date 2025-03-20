9aa0ba46ed9193ee066aefc50d47dd03e7dd4cbd
mikitadk41 <dk41.kohan@lll.kpi.ua>
2025-03-20 13:28:23 +0200

    Create main.c

---
+++ DK41/Kokhan M/LAB2/main.c
@@ -0,0 +1,34 @@
+// main.c
+#include "list.h"
+#include <stdio.h>
+
+int main() {
+    List* myList = createTwoSidedMallocList();
+
+    if (myList == NULL) {
+        fprintf(stderr, "Failed to create list. Exiting.\n");
+        return 1;
+    }
+
+    append(myList, 1);
+    append(myList, 2);
+    append(myList, 3);
+    append(myList, 2);
+    append(myList, 9);
+    append(myList, 2);
+    append(myList, 5);
+
+    printf("Started list:\n");
+    printList(myList);
+
+    int elementToDouble = 9;
+
+    doubleElement(myList, elementToDouble);
+
+    printf("\nList after doudle %d:\n", elementToDouble);
+    printList(myList);
+
+    freeList(myList);
+
+    return 0;
+}
