0b6c369ded14b35c3270e5ba6f5c72eef99af778
mikitadk41 <dk41.kohan@lll.kpi.ua>
2025-03-20 14:50:38 +0200

    Update list.h

--- DK41/Kokhan M/LAB2/list.h
+++ DK41/Kokhan M/LAB2/list.h
@@ -9,7 +9,7 @@ typedef struct Node {
 void init_list(Node** head);
 void add_to_list(Node** head, int value);
 void print_list(Node* head);
-int reverse_between(Node* head, int target);
+int reverse_between(Node** head, int target);
 void free_list(Node* head);
 
 #endif
