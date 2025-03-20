248bec1d932dd419ecfe8910434f6f915c2de6c9
mikitadk41 <dk41.kohan@lll.kpi.ua>
2025-03-20 14:09:34 +0200

    Update main.c

--- DK41/Kokhan M/LAB2/main.c
+++ DK41/Kokhan M/LAB2/main.c
@@ -1,33 +1,66 @@
-#include "list.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
 
 int main() {
-    List* myList = createTwoSidedMallocList();
+    Node* list;
+    int choice, value, target;
 
-    if (myList == NULL) {
-        fprintf(stderr, "Failed to create list. Exiting.\n");
-        return 1;
-    }
+    init_list(&list);
 
-    append(myList, 1);
-    append(myList, 2);
-    append(myList, 3);
-    append(myList, 2);
-    append(myList, 9);
-    append(myList, 2);
-    append(myList, 5);
+    do {
+        printf("\nMenu:\n");
+        printf("1. Add an element to the list\n");
+        printf("2. Print the list\n");
+        printf("3. Reverse elements between the first and last occurrence of a given element\n");
+        printf("4. Exit\n");
+        printf("Select an action (1-4): ");
+        if (scanf("%d", &choice) != 1) {
+            printf("Invalid choice! Please enter a number between 1 and 4.\n");
+            while(getchar() != '\n');
+            continue;
+        }
 
-    printf("Started list:\n");
-    printList(myList);
+        switch(choice) {
+            case 1:
+                printf("Enter a number to add to the list: ");
+                if (scanf("%d", &value) != 1) {
+                    printf("Invalid value! Please enter an integer.\n");
+                    while(getchar() != '\n');
+                    break;
+                }
+                add_to_list(&list, value);
+                printf("Number %d added to the list.\n", value);
+                break;
 
-    int elementToDouble = 9;
+            case 2:
+                printf("List: ");
+                print_list(list);
+                break;
 
-    doubleElement(myList, elementToDouble);
+            case 3:
+                printf("Enter a number to reverse elements between its first and last occurrence: ");
+                if (scanf("%d", &target) != 1) {
+                    printf("Invalid value! Please enter an integer.\n");
+                    while(getchar() != '\n');
+                    break;
+                }
+                if (reverse_between(list, target)) {
+                    printf("Elements between the first and last occurrence of %d have been reversed.\n", target);
+                } else {
+                    printf("Number %d does not appear in the list or appears less than twice.\n", target);
+                }
+                break;
 
-    printf("\nList after doudle %d:\n", elementToDouble);
-    printList(myList);
+            case 4:
+                printf("Exiting the program...\n");
+                free_list(list);
+                break;
 
-    freeList(myList);
+            default:
+                printf("Invalid choice! Please enter a number between 1 and 4.\n");
+        }
+    } while(choice != 4);
 
     return 0;
 }
