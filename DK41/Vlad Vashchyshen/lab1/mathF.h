a214f12cfa3c440c076d2878cd863df15b2b038d
mgVVkpi <vashchyshenkpisss@gmail.com>
2025-04-23 19:45:37 +0300

    Update mathF.h

--- DK41/Vlad Vashchyshen/lab1/mathF.h
+++ DK41/Vlad Vashchyshen/lab1/mathF.h
@@ -1,1 +1,11 @@
+#ifndef MATHFX_H
+#define MATHFX_H
+
+// Функція для обчислення суми степенів 2^(D+A)
+double calculateSum(int A);
+
+// Функція для обчислення основного виразу Q
+double calculateQ(int A, int B, int C);
+
+#endif
 
