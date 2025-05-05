bc08b5c2582f93f025ec2673e3ebf10c3e1878db
mgVVkpi <vashchyshenkpisss@gmail.com>
2025-05-05 11:16:26 +0300

    Update list.h

--- DK41/Vlad Vashchyshen/lab2/list.h
+++ DK41/Vlad Vashchyshen/lab2/list.h
@@ -1,1 +1,15 @@
+#ifndef LIST_H
+#define LIST_H
 
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+Node* createNode(int data);
+Node* createCircularList(int n);
+void printList(Node* head);
+void deleteEachThird(Node** headRef);
+void freeList(Node* head);
+
+#endif
