42130b58f15138ac45dd5f12ba2aa4b88ed5724f
mikitadk41 <dk41.kohan@lll.kpi.ua>
2025-05-12 13:54:01 +0300

    Create labtree.c

---
+++ DK41/Kokhan M/LAB3/labtree.c
@@ -0,0 +1,99 @@
+#include "labtree.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+TreeNode* createNode(double data) {
+    TreeNode* newNode = (TreeNode*)malloc(sizeof(TreeNode));
+    if (newNode == NULL) {
+        perror("Error allocating memory for new node");
+        exit(EXIT_FAILURE);
+    }
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+TreeNode* insertNode(TreeNode* root, double data) {
+    if (root == NULL) {
+        return createNode(data);
+    }
+
+    if (data < root->data) {
+        root->left = insertNode(root->left, data);
+    } else {
+        root->right = insertNode(root->right, data);
+    }
+
+    return root;
+}
+
+static TreeNode* minValueNode(TreeNode* node) {
+    TreeNode* current = node;
+    while (current != NULL && current->left != NULL) {
+        current = current->left;
+    }
+    return current;
+}
+
+TreeNode* deleteNode(TreeNode* root, double data) {
+
+    if (root == NULL) {
+        return root;
+    }
+    if (data < root->data) {
+        root->left = deleteNode(root->left, data);
+    } else if (data > root->data) {
+        root->right = deleteNode(root->right, data);
+    }
+    else {
+        //Випадок 1: Вузол без нащадків або з одним нащадком
+        if (root->left == NULL) {
+            TreeNode* temp = root->right;
+            free(root);
+            return temp;
+        } else if (root->right == NULL) {
+            TreeNode* temp = root->left;
+            free(root);
+            return temp;
+        }
+
+        //Випадок 2: Вузол з двома нащадками
+        TreeNode* temp = minValueNode(root->right);
+
+        root->data = temp->data;
+
+        root->right = deleteNode(root->right, temp->data);
+    }
+    return root;
+}
+
+TreeNode* searchNode(TreeNode* root, double data) {
+    if (root == NULL || root->data == data) {
+        return root;
+    }
+
+    if (data < root->data) {
+        return searchNode(root->left, data);
+    }
+
+    return searchNode(root->right, data);
+}
+
+void deleteTree(TreeNode* root) {
+    if (root == NULL) {
+        return;
+    }
+
+    deleteTree(root->left);
+    deleteTree(root->right);
+    free(root);
+}
+
+void printInOrder(TreeNode* root) {
+    if (root != NULL) {
+        printInOrder(root->left);
+        printf("%.2f ", root->data);
+        printInOrder(root->right);
+    }
+}
